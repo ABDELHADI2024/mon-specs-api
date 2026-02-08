131092044c173991b9525fd5e57510899a99da6d
ABDELHADI2024 <158781565+ABDELHADI2024@users.noreply.github.com>
2026-02-08 21:49:36 +0100

    Integrate v2 routes into the main router
    Added v2 routes to the main router.

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const searchRoutes = require("./search");
 const scrapeRoutes = require("./scrape");
 const phoneRoutes = require("./phone");
-
+const v2Routes = require("./v2/index"); // On importe la version 2 que nous avons améliorée
 
 const indexController = require('../controllers/index')
 
@@ -11,5 +11,8 @@ router.get("/", indexController.index);
 router.use("/brands", phoneRoutes);
 router.use("/search", searchRoutes);
 router.use("/scrape", scrapeRoutes);
+
+// LA LIGNE MAGIQUE : On active enfin le dossier v2
+router.use("/v2", v2Routes);
 
 module.exports = router;
