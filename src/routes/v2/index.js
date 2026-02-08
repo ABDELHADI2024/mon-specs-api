9e523fdbff90d3871713b307d9e13eff29f62d92
ABDELHADI2024 <158781565+ABDELHADI2024@users.noreply.github.com>
2026-02-08 21:44:06 +0100

    Refactor routes and add specs endpoint
    Refactored routes to remove duplicate controller imports and added a new route for specs.

--- src/routes/v2/index.js
+++ src/routes/v2/index.js
@@ -1,20 +1,21 @@
 const express = require("express");
 const router = express.Router();
 
+const brandController = require('../../controllers/v2/brandController');
+const specController = require('../../controllers/v2/specController');
+const searchController = require('../../controllers/v2/searchController');
+const miscController = require('../../controllers/v2/miscController');
 
-const indexController = require('../../controllers/v2/index')
-const brandController = require('../../controllers/v2/brandController')
-const specController = require('../../controllers/v2/specController')
-const searchController = require('../../controllers/v2/searchController')
-const miscController = require('../../controllers/v2/miscController')
-
-router.get("/", indexController.index);
+// Routes organisées
 router.get("/brands", brandController.index);
 router.get("/brands/:slug", brandController.show);
 router.get("/search", searchController.index);
 router.get("/latest", miscController.index);
 router.get("/top-by-interest", miscController.topInterest);
 router.get("/top-by-fans", miscController.topFans);
-router.get("/:slug", specController.index);
+
+// Route pour les caractéristiques d'un téléphone (C'est celle qu'on utilise)
+// On ajoute 'specs' dans l'URL pour que ce soit clair : /v2/specs/nom-du-phone
+router.get("/specs/:slug", specController.index);
 
 module.exports = router;
